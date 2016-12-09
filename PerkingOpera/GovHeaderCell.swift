4006f1f07bae0a2e4692c647de9aaddc82c14784
meng <hughmeng@gmail.com>
2016-12-09 17:00:14 +0800

    New Gov cell view file

---
+++ PerkingOpera/GovHeaderCell.swift
@@ -0,0 +1,25 @@
+//
+//  GovHeaderCell.swift
+//  PerkingOpera
+//
+//  Created by admin on 09/12/2016.
+//  Copyright © 2016 Wayne Meng. All rights reserved.
+//
+
+import UIKit
+
+class GovHeaderCell: UITableViewCell {
+    
+    @IBOutlet weak var flowNameLabel: UILabel!
+    
+    @IBOutlet weak var depNameLabel: UILabel!
+
+    @IBOutlet weak var creatorLabel: UILabel!
+    
+    @IBOutlet weak var createDateLabel: UILabel!
+    
+    @IBOutlet weak var remarkLabel: UILabel!
+    
+    
+    
+}
